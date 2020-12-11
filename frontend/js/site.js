ba31792bf3f35595dcf78cbbca6dc93d90f22420
StardustDL <stardustdl@163.com>
2020-12-11 13:38:13 +0800

    add layout for frontend

--- frontend/js/site.js
+++ frontend/js/site.js
@@ -1,15 +1,42 @@
 const App = {
     data() {
         return {
+            currentStep: 2,
+            model: {
+                selectedStep: 0,
+            }
         }
     },
     methods: {
-
+        getStepStatus(id) {
+            if (id < this.currentStep) {
+                return "finish";
+            }
+            else if (id == this.currentStep) {
+                return "process";
+            }
+            else if (id > this.currentStep) {
+                return "wait";
+            }
+            return "wait";
+        }
     },
     computed: {
+        selectedStep: {
+            get() {
+                return this.model.selectedStep;
+            },
+            set(newValue) {
+                if (newValue > this.currentStep) {
+                    this.$message.warning('Please complete this step first.');
+                    newValue = this.currentStep;
+                }
+                this.model.selectedStep = newValue;
+            }
+        },
     },
     mounted() {
     }
 };
-var app = Vue.createApp(App);
+var app = Vue.createApp(App).use(antd);
 app.mount("#app");
